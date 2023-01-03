e5412fd12a3e93fd0f01f4020fcb9d2c2f408099
demettriss <demettriss@gmail.com>
2023-01-03 17:41:37 +0300

    Game: "Calculator"

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/demettriss/php-project-45/workflows/hexlet-check/badge.svg)](https://github.com/demettriss/php-project-45/actions)
 <a href="https://codeclimate.com/github/demettriss/php-project-45/maintainability"><img src="https://api.codeclimate.com/v1/badges/679f9e42545decffce7d/maintainability" /></a>
+
+### Игра: "Проверка на чётность"
+~~~
+./bin/brain-even
+
+Welcome to the Brain Games!
+May I have your name? Sam
+Hello, Sam!
+Answer "yes" if the number is even, otherwise answer "no".
+Question: 15
+Your answer: no
+Correct!
+Question: 6
+Your answer: yes
+Correct!
+Question: 7
+Your answer: no
+Correct!
+Congratulations, Sam!
+~~~
+
+### Игра: "Калькулятор"
+~~~
+./bin/brain-calc
+
+Welcome to the Brain Games!
+May I have your name? Sam
+Hello, Sam!
+What is the result of the expression?
+Question: 4 + 10
+Your answer: 14
+Correct!
+Question: 25 - 11
+Your answer: 14
+Correct!
+Question: 25 * 7
+Your answer: 175
+Correct!
+Congratulations, Sam!
+~~~
---
+++ src/Games/Calc.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BrainGames\Games\Calc;
+
+use function cli\line;
+use function cli\prompt;
+
+const OPERANDS = ["+", "-", "*"];
+
+function run(): void
+{
+    global $userName;
+
+    line('What is the result of the expression?');
+    $right = 0;
+    while (true) {
+        if ($right == 3) {
+            line("Congratulations, {$userName}!");
+            break;
+        }
+        $first = rand(1, 50);
+        $operand = OPERANDS[array_rand(OPERANDS)];
+        $second = rand(1, 50);
+        $question = "{$first} {$operand} {$second}";
+        line("Question: {$question}");
+        $answer = prompt('Your answer');
+        $correctAnswer = checkAnswer($question);
+        if ($answer != $correctAnswer) {
+            $right = 0;
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, {$userName}!");
+        } else {
+            $right++;
+            line("Correct!");
+        }
+    }
+}
+
+function checkAnswer(string $question): ?int
+{
+    try {
+        eval("\$result = $question;");
+    } catch (\Exception $e) {
+        $result = null;
+    }
+    return $result;
+}
